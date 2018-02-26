7af30bd1ac0c57c3fc7bf649a1d2e53c12e9368a
cooney.shawn@gmail.com <cooney.shawn@gmail.com>
2018-02-26 10:53:05 -0500

    Created a few parse functions.

--- web/src/NumUtil.js
+++ web/src/NumUtil.js
@@ -33,6 +33,33 @@ NumUtil.Core = NumUtil.prototype = {
         }
         return subVal;
     },
+    // Parses a float to the specified precision and removes trailing zeros
+    ParseFloatNoZerosWithoutCommas: function (value, precision) {
+        if (StrUtil.Core.IsNullOrEmpty(value)) {
+            return value;
+        }
+        // Only replace commas if they exist
+        if (value.indexOf(',') >= 0) {
+            value = value.replace(/,/g, StrUtil.Core.EmptyString);
+        }
+        if (isNaN(value)) {
+            return value;
+        }
+        var nVal = parseFloat(value);
+        var fVal = nVal.toFixed(precision);
+        var tVal = fVal.replace(/^0+|0+$/g, StrUtil.Core.EmptyString);
+        if (isNaN(tVal) || StrUtil.Core.IsNullOrEmpty(tVal)) {
+            return value;
+        }
+        var subVal = tVal;
+        if (tVal.charAt(tVal.length - 1) === '.') {
+            subVal = tVal.substring(0, (tVal.length - 1));
+        }
+        if (subVal.charAt(0) === '.') {
+            return ('0' + subVal);
+        }
+        return subVal;
+    },
     ParseFloatWithCommas: function (value) {
         var parts = value.toString().split(".");
         parts[0] = parts[0].replace(/,/g, StrUtil.Core.EmptyString);
@@ -43,5 +70,16 @@ NumUtil.Core = NumUtil.prototype = {
         var parts = value.toString().split(".");
         parts[0] = parts[0].replace(/,/g, StrUtil.Core.EmptyString);
         return parts.join(".");
+    },
+    // Attempts to parse an integer.  If it fails, it will use the default value
+    ParseIntWithDefault: function (value, defaultVal) {
+        var num = parseInt(value);
+        if (isNaN(num)) {
+            var defNum = parseInt(defaultVal);
+            if (isNaN(defNum)) {
+                return defaultVal;
+            }
+            return defNum;
+        }
     }
 };
